83196f8f4a00d32e49755eb5388e6cb739c2fabd
Artem <tema1997s@gmail.com>
2023-04-21 23:22:57 +0300

    .gitignore changed

--- src/main/java/ru/netology/javaqa/services/MonthCalculateService.java
+++ src/main/java/ru/netology/javaqa/services/MonthCalculateService.java
@@ -3,7 +3,7 @@ package ru.netology.javaqa.services;
 public class MonthCalculateService {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0;
-        int money = 0; // количество денег на счету
+        int money = 0;
         for (int month = 0; month < 12; month++) {
             if (money >= threshold) {
                 count++;
